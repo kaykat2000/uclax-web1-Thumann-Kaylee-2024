d9492d248a92fcf213815b9b356cdc61c7d3f0e2
Kaylee Thumann <kaykat2000@gmail.com>
2024-05-13 20:13:23 -0700

    All Essays (Unbulleted/unformatted)

--- src/Pages/CourseWork/Essays/Essays.jsx
+++ src/Pages/CourseWork/Essays/Essays.jsx
@@ -213,21 +213,88 @@ const Essays = () => {
                     "Q3 What is the difference between JQuery and React?."
                 }
             >
-                Answer 3 goes here.
+                JQuery JQuery serves as a utility library, streamlining web app
+                development by facilitating JavaScript use jQuery is a
+                lightweight JavaScript library primarily used for DOM
+                manipulation and simplifying AJAX requests. It's ideal for
+                enhancing web page interactivity.  jQuery has a lower learning
+                curve, making it accessible for beginners with its simple
+                syntax. It excels at quick and straightforward DOM
+                manipulations. With its component-based structure and JSX syntax
+                 jQuery remains useful for simpler tasks, React's ability to
+                handle complex applications and its compatibility with evolving
+                industry standards position it as a more future-proof choice.
+                React a library that allows the embedding of HTML within
+                JavaScript. a powerful JavaScript library for building user
+                interfaces, efficient rendering, emphasizing a component-based
+                architecture, and state management. React excels in developing
+                single-page applications, offering a more structured and
+                scalable approach than jQuery. React has a steeper learning
+                curve. However, once developers become proficient, React's
+                comprehensive documentation, strong community, and reusable
+                component patterns contribute to a more robust and maintainable
+                codebase. React is considered more sustainable for future
+                development. Its component-based architecture, virtual DOM, and
+                strong community support align with modern development
+                practices. created by Facebook -interactive UI elements
+                re-usable components component based All JavaScript is within it
+                SPAs More scalable JSX: Javascript XML - makes it easier to
+                build out content Doesn’t need to reroute elements DOM
+                Manipulates elements
+                https://www.simplilearn.com/jquery-vs-react-article#:~:text=jQuery%20serves%20as%20a%20utility,they%20can%20be%20interchangeably%20used.
             </SingleEssay>
             <SingleEssay
                 question={
                     "Q4 What is the difference between a Front-End and Back-End Developer?"
                 }
             >
-                Answer 3 goes here.
+                Front-End creates websites and apps that allow users to access
+                and interact with the site or app. languages: HTML, CSS, and
+                JavaScript What’s seen on the first page translates designs to
+                interactive layouts Back-End build and maintain the mechanisms
+                that process data and perform actions on websites languages:
+                Programming Languages Python PHP JavaScript Ruby Java C#
+                Frameworks (the libraries of back-end programming languages that
+                help to build the server configuration. They tend to be linked
+                with programming languages, so if you are familiar with Python,
+                you’ll also know Flask, Django, or another Python-based
+                framework, and so on.): Laravel Django Spring Ruby on Rails
+                Meteor Node.js Servers (stack and recover data from databases,
+                as back-end programming controls access to this information,
+                including storage and recovery. The database stores and
+                organizes the client’s data so that it can be easily arranged
+                and recovered. This database then runs on a server that provides
+                data upon request.): Apache NGINX Lighttpd Microsoft IIS API
+                request responses back-end developers are involved in data
+                storage, security, and other server-side functions that aren’t
+                seen. Gives the API own validation front end back end gets the
+                data from the server QA (quality assurance), Front end, back end
+                https://www.coursera.org/articles/back-end-developer
+                https://www.coursera.org/articles/front-end-developer
             </SingleEssay>
             <SingleEssay
                 question={
                     "Q5 In React, what are the different ways we can link to resources (e.g. Root Relative vs Document Relative)?"
                 }
             >
-                Answer 3 goes here.
+                Absolute Hyperlinks complete addresses that contain all the
+                elements of a URL always start with http:// followed by domain
+                used when linking to pages outside of the current site that have
+                a different domain name. Relative hyperlinks addresses that are
+                relative to the current domain or location ../Folder
+                2/page.html, where the ../ tells the browser you want to go out
+                of the current folder and into a new one easily update them if
+                you choose to move files around move the files around outside of
+                the Expression Web program they break more complex import paths
+                flexibility & portability . in front Root-relative hyperlink
+                subset of relative hyperlinks all the links are assumed to start
+                from the root folder (domain name) of the site browser applies
+                only the domain to the beginning of this link Because they refer
+                to the root of the site rather than the current location of the
+                page they are placed in, they work regardless of where the file
+                is placed as long as they remain under the right domain. easier
+                to copy from file to file @ in front
+                https://mor10.com/html-basics-hyperlink-syntax-absolute-relative-and-root-relative/
             </SingleEssay>
             <SingleEssay
                 question={
